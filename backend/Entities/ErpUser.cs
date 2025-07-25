13721a4f822bdd66c7aba466f80334162eeff266
ElsiumCa <173598971+ElsiumCa@users.noreply.github.com>
2025-07-25 14:21:25 +0300

    ErpUser Propları belirlendi.

--- backend/Entities/ErpUser.cs
+++ backend/Entities/ErpUser.cs
@@ -6,7 +6,26 @@ namespace Entities
     //Login ve Register ve diğer her şey için bir DTO oluşturun.
     public class ErpUser : IdentityUser
     {
-        // Ek özellikler buraya eklenebilir
+
+        //Telefon numarası ve E-posta IdentityUser tarafından sağlanıyor.
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string InstitutionName { get; set; } = string.Empty;
+
+        public string? RegistrationKey { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime? LastActivityTime { get; set; }
+
+        public bool RememberMe { get; set; } = false;
+
+        public string FullName => $"{FirstName} {LastName}".Trim();
+
+
     }
 
 
