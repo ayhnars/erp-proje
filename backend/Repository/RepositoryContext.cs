742b617dcb0feda6f8f062743279e76e04eb51ce
ElsiumCa <173598971+ElsiumCa@users.noreply.github.com>
2025-07-25 14:00:17 +0300

    Refactor code structure for improved readability and maintainability

--- backend/Repository/RepositoryContext.cs
+++ backend/Repository/RepositoryContext.cs
@@ -1,6 +1,16 @@
-﻿namespace Repository;
+﻿using Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-public class Class1
+namespace Repository
 {
+    public class RepositoryContext : IdentityDbContext<ErpUser>
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+            : base(options)
+        {
+        }
 
+        // Add DbSet properties for your entities here
+    }
 }
