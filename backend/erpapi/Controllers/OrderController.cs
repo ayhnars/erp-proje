eefd5d74c96ade52d1686eda4e6104fbb969ddc7
ayhan arslan <ayhan1113arslan@gmail.com>
2025-08-06 15:27:15 +0300

    register branchinde yaptığım değişiklikler

---
+++ backend/erpapi/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using erpapi.Services.Contracts;
+using erpapi.Models;
+using erpapi.Services;
+
+namespace erpapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var orders = _orderService.GetAllOrders();
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Order order)
+        {
+            _orderService.CreateOrder(order);
+            return Ok(order);
+        }
+    }
+}
--- backend/erpapi/obj/Release/net9.0/erpapi.AssemblyInfo.cs
+++ backend/erpapi/obj/Release/net9.0/erpapi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("erpapi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f7e9ca546ad64591487d1ce7e2b70872874bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("erpapi")]
