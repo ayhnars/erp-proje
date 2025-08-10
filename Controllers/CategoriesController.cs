3a2471d6704f447b511e664d0ed5c2d314f8488a
EmreEmir-17 <emreemir7214@gmail.com>
2025-08-10 18:50:35 +0300

    Yaptığım değişiklikler: Controller, Model güncellemeleri

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Erp_sistemi1.Data;
+using Erp_sistemi1.Models;
+
+namespace Erp_sistemi1.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private UygulamaDbContext db = new UygulamaDbContext();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var categories = db.Categories.ToList();
+            return View(categories);
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var category = db.Categories.Find(id);
+            if (category == null)
+                return HttpNotFound();
+
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var category = db.Categories.Find(id);
+            if (category == null)
+                return HttpNotFound();
+
+            return View(category);
+        }
+
+        // POST: Categories/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var category = db.Categories.Find(id);
+            if (category == null)
+                return HttpNotFound();
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Data/UygulamaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Erp_sistemi1.Models;
+
+namespace Erp_sistemi1.Data
+{
+    public class UygulamaDbContext : DbContext
+    {
+        public UygulamaDbContext() : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
