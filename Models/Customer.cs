c5186e45ac7d7a25b1e5b25e66e2207de8a6d38d
EmreEmir-17 <emreemir7214@gmail.com>
2025-08-18 19:38:16 +0300

    Customers API eklendi ve test edildi

---
+++ Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Erp_sistemi1.Models
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        public int CompanyID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string CustomerCode { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string CustomerName { get; set; }
+
+        [MaxLength(20)]
+        public string Phone { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
