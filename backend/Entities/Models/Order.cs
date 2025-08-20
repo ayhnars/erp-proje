ed61960637e77fa88c946b888594e5143c11e281
ElsiumCa <173598971+ElsiumCa@users.noreply.github.com>
2025-08-20 17:18:56 +0300

    yanlışlıkla silinen dosyalar geri alındı.

--- backend/Entities/Models/Order.cs
+++ backend/Entities/Models/Order.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-<<<<<<< HEAD
-using erpapi.Models;
-
-namespace erpapi.Models
-=======
 
 namespace Entities.Models
->>>>>>> order
 {
     public enum PaymentMethod
     {
@@ -25,10 +19,7 @@ namespace Entities.Models
         Cancelled
     }
 
-<<<<<<< HEAD
-=======
     [Table("Orders")]
->>>>>>> order
     public class Order
     {
         [Key]
@@ -40,19 +31,6 @@ namespace Entities.Models
         [ForeignKey("Customer")]
         public int CustomerID { get; set; }
 
-<<<<<<< HEAD
-        public DateTime OrderDate { get; set; } = DateTime.Now;
-
-        public DateTime DeliveryDate { get; set; }
-
-        public PaymentMethod PaymentMethod { get; set; }
-
-        public OrderStatus Status { get; set; }
-
-        // Navigation Properties (isteğe bağlı)
-        // public Company Company { get; set; }
-        // public Customer Customer { get; set; }
-=======
         // Sipariş oluşturulduğunda otomatik dolsun
         public DateTime OrderDate { get; set; } = DateTime.Now;
 
@@ -67,6 +45,5 @@ namespace Entities.Models
         // public Company? Company { get; set; }
         // public Customer? Customer { get; set; }
         // public ICollection<OrderItem>? Items { get; set; }
->>>>>>> order
     }
 }
